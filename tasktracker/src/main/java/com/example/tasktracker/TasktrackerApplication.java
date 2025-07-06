e3769760501d991798a28ed0dfcb9c20cbada891
Naheer Fatima <nnolas33@asu.edu>
2025-07-06 18:21:33 -0400

    Modified task tracker app

--- tasktracker/src/main/java/com/example/tasktracker/TasktrackerApplication.java
+++ tasktracker/src/main/java/com/example/tasktracker/TasktrackerApplication.java
@@ -5,7 +5,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class TaskTrackerApplication {
+
 	public static void main(String[] args) {
 		SpringApplication.run(TaskTrackerApplication.class, args);
 	}
+
 }
